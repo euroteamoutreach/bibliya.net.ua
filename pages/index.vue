436486bbba49e9c88a902572b4c7f09117b002d6
Joshua Steele <joshukraine@gmail.com>
2018-07-06 11:53:09 +0300

    Add Chronological Approach section.

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ div
 
   //- Solid Foundation
   section.py-10(class="sm:py-16 lg:py-20")
-    .container.flex.mx-auto(class='lg:max-w-xl')
+    .container.flex.mx-auto(class="lg:max-w-xl")
       .hidden(class="md:block md:w-1/3 md:mr-10")
         img(src="~/assets/images/lesson-10-combo-500w.png")
       .flex-1
@@ -19,13 +19,37 @@ div
         .mb-4.mx-auto(class="w-3/4 sm:w-2/3 md:hidden")
           img(src="~/assets/images/lesson-10-combo-500w.png")
         p(v-html="foundationContent")
-</template>
 
-<style>
-.bf-bg-img {
-  background-image: url("~/assets/images/bf-bg-fade-1500w.jpg");
-}
-</style>
+  //- Chronological Approach
+  section.py-10.ark-bg-fade.bg-cover.bg-center(class="sm:py-16 md:py-24 xl:py-32")
+    .container.flex.mx-auto(class="lg:max-w-xl")
+      div(class="md:mr-6 md:w-1/2")
+        h2.text-center.text-white {{ chronHeading }}
+        .mt-6.mb-2.mx-auto(class="w-3/4 md:hidden")
+          img(src="~/assets/images/noah-timeline-combo-500w.png")
+        p.text-white.text-center(v-html="chronContent")
+      .hidden.ml-6(class="md:block w-1/2")
+        img(src="~/assets/images/noah-timeline-combo-500w.png")
+
+  //- Temp spacing section.
+  section.py-32
+    .container
+      p.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      p.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      p.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</template>
 
 <script>
 import debounce from 'lodash.debounce'
@@ -36,6 +60,8 @@ export default {
       enrollNow: 'Підписатися',
       foundationHeading: 'Закладіть міцну основу',
       foundationContent: '<em>Біблія понад усе</em> — це безплатний курс дистанційного навчання на основі першої книги Біблії — Книги Буття. Ця унікальна книга представляє нам неймовірну історію Божих взаємин з людьми, навчаючи найважливіших біблійних доктрин через незабутні історії та приклади з реального життя. У наших уроках, окрім Книги Буття, ми також звертаємо увагу на основні паралельні уривки з Нового Завіту, щоб Ви могли отримати чітке та зв\'язне розуміння Біблії в цілому.',
+      chronHeading: 'Випробуйте хронологічний підхід',
+      chronContent: 'Як і будь-яку іншу книжку, Біблію потрібно читати з першого розділу. Багатьом людям важко зрозуміти її, бо їм відомі лише окремі шматки не пов\'язаної між собою інформації. Забудьте про цю плутанину, тому що <em>Біблія понад усе</em> поведе Вас у подорож часом, відкриваючи любов нашого Творця до порядку і мети. Здивуйтеся точністю Божого плану, і нехай Він розкаже Вам історію спасіння, що була написана вже на самому початку.',
       scrollHandler: debounce(this.navScrollFade, 100, {
         'leading': true
       })
@@ -68,3 +94,13 @@ export default {
   }
 }
 </script>
+
+<style>
+.bf-bg-img {
+  background-image: url("~/assets/images/bf-bg-fade-1500w.jpg");
+}
+
+.ark-bg-fade {
+  background-image: url("~/assets/images/noahs-ark-bg-fade-1500w.jpg");
+}
+</style>
