6aa28da9ce94520bf0b92250ca296b13fc666bc1
Joshua Steele <joshukraine@gmail.com>
2018-07-06 13:20:29 +0300

    Spacing and alignment tweaks.

--- pages/index.vue
+++ pages/index.vue
@@ -11,36 +11,36 @@ div
   //- Solid Foundation
   section.py-10(class="sm:py-16 lg:py-20")
     .container.flex.mx-auto(class="lg:max-w-xl")
-      .hidden(class="md:block md:w-1/3 md:mr-10")
+      .hidden(class="lg:block lg:w-1/3 lg:mr-10 lg:pt-2")
         img(src="~/assets/images/lesson-1-combo-500w.jpg")
       .flex-1
-        h2.text-center(class="md:text-left")
+        h2.text-center(class="lg:text-left")
           | {{ foundationHeading }}
-        .mb-4.mx-auto(class="w-3/4 sm:w-2/3 md:hidden")
-          img(src="~/assets/images/lesson-1-combo-500w.jpg")
+        .mb-3.mt-8.mx-auto(class="w-3/4 sm:w-2/3 lg:hidden")
+          img.block(src="~/assets/images/lesson-1-combo-500w.jpg")
         p(v-html="foundationContent")
 
   //- Chronological Approach
   section.py-10.ark-bg-fade.bg-cover.bg-center(class="sm:py-16 md:py-24 xl:py-32")
     .container.flex.mx-auto(class="lg:max-w-xl")
-      div(class="md:mr-6 md:w-1/2")
+      div(class="lg:mr-6 lg:w-1/2")
         h2.text-center.text-white {{ chronHeading }}
-        .mt-6.mb-2.mx-auto(class="w-3/4 md:hidden")
+        .mt-6.mb-2.mx-auto(class="w-3/4 lg:hidden")
           img(src="~/assets/images/noah-timeline-combo-500w.png")
         p.text-white.text-center(v-html="chronContent")
-      .hidden.ml-6(class="md:block w-1/2")
+      .hidden.ml-6(class="lg:block lg:pt-4 w-1/2")
         img(src="~/assets/images/noah-timeline-combo-500w.png")
 
   //- Gain Independence
   section.py-10(class="sm:py-16 md:py-24")
-    .container.flex.mx-auto(class="lg:max-w-lg")
-      .hidden(class="md:block md:w-2/5 lg:w-1/3 md:mr-10")
+    .container.flex.mx-auto(class="lg:max-w-xl")
+      .hidden(class="lg:block lg:mr-16 lg:w-1/3 lg:pt-4 xl:mr-20")
         img.block.rounded.shadow-md(src="~/assets/images/phillip-Bible-700w.jpg")
       .flex-1
         h2.text-center {{ gainIndHeading }}
-        .my-8.mx-auto.max-w-xs(class="w-3/4 md:hidden")
+        .my-8.mx-auto.max-w-xs(class="w-3/4 sm:my-12 lg:hidden")
           img.block.rounded.shadow-md(src="~/assets/images/phillip-Bible-700w.jpg")
-        p(v-html="gainIndContent")
+        p(class="md:text-center" v-html="gainIndContent")
 
   //- Study from Home
   section.py-12.study-bg-fade.bg-cover.bg-center(class="sm:py-16 md:py-24 lg:py-32")
@@ -81,6 +81,8 @@ export default {
       chronContent: 'Як і будь-яку іншу книжку, Біблію потрібно читати з першого розділу. Багатьом людям важко зрозуміти її, бо їм відомі лише окремі шматки не пов\'язаної між собою інформації. Забудьте про цю плутанину, тому що <em>Біблія понад усе</em> поведе Вас у подорож часом, відкриваючи любов нашого Творця до порядку і мети. Здивуйтеся точністю Божого плану, і нехай Він розкаже Вам історію спасіння, що була написана вже на самому початку.',
       gainIndHeading: 'Здобудьте свою незалежність',
       gainIndContent: 'Чи Вам доводилось бути в замішанні від того, що Ви намагалися збагнути тлумачення різних конфесій, які суперечать одне одному? Чия тут правда? Як не стати ошуканим? <em>Біблія понад усе</em> навчить Вас самостійно досліджувати Писання, використовуючи прості і випробувані часом літературні прийоми. З кожною новою відповіддю, знайденою для себе, Ви будете здобувати впевненість у тому, що Ваші висновки правильні — і не тому, що хтось так сказав, а тому, що так каже Біблія.',
+      studyHeading: 'Навчайтеся вдома',
+      studyContent: 'Без аудиторій, без їзди, без зустрічей. Ми надсилаємо кожен урок Вам додому цілком безплатно. Просто прочитайте урок у своєму темпі, дайте відповіді на запитання і надішліть їх нам! Ми оцінимо Вашу роботу і повернемо ці відповіді разом із наступним уроком.',
       scrollHandler: debounce(this.navScrollFade, 100, {
         'leading': true
       })
@@ -121,5 +123,9 @@ export default {
 
 .ark-bg-fade {
   background-image: url("~/assets/images/noahs-ark-bg-fade-1500w.jpg");
+}
+
+.study-bg-fade {
+  background-image: url("~/assets/images/study-home-bg-fade-1500w.jpg");
 }
 </style>
