7cd10082ef3a1801ea62b886684575b059614c66
Joshua Steele <joshukraine@gmail.com>
2019-11-27 09:43:39 +0200

    Convert pro nas page to html.

--- pages/pro-nas/index.vue
+++ pages/pro-nas/index.vue
@@ -1,27 +1,20 @@
-<template lang="pug">
-.container.pb-12.pt-24(class='md:pb-16 md:pt-32')
-  .max-w-md.mx-auto.text-center
-    h1 Про нас
-    p.m-0
-      | Euro Team Outreach — це благодійна неприбуткова організація,
-      | діяльність якої пов'язана з поширенням Євангелії Ісуса Христа. Ми не
-      | належимо до жодної церкви чи конфесії, а зосереджені на тому, щоб
-      | заохотити людей з різних країн і культур читати Біблію. Ми віримо, що
-      | Біблія — це святе Слово Боже і найвищий авторитет для нашого життя.
-    div.py-6(class='md:py-12')
-      a(href="https://euroteamoutreach.org/" target="_blank")
-        img.rounded.shadow-md(src="~/assets/images/eto-cover-700w.jpg")
-    p
-      | Щоб дізнатися більше про нашу роботу, будь ласка, завітайте на наш
-      | основний сайт (англійською мовою):&nbsp;
-      a(href='https://euroteamoutreach.org/' target='_blank') euroteamoutreach.org
-
-    hr.border-b.border-grey-light.my-12
-
-    button.btn.btn-blue(@click='downloadPdf')
-      span.mr-2 У що ми віримо
-      font-awesome-icon(:icon="['fas', 'cloud-download-alt']")
-
+<template>
+  <div class="container pb-12 pt-24 md:pb-16 md:pt-32">
+    <div class="max-w-md mx-auto text-center">
+      <h1>Про нас</h1>
+      <p class="m-0">
+        Euro Team Outreach — це благодійна неприбуткова організація, діяльність якої пов'язана з поширенням Євангелії Ісуса Христа. Ми не належимо до жодної церкви чи конфесії, а зосереджені на тому, щоб заохотити людей з різних країн і культур читати
+        Біблію. Ми віримо, що Біблія — це святе Слово Боже і найвищий авторитет для нашого життя.
+      </p>
+      <div class="py-6 md:py-12">
+        <a href="https://euroteamoutreach.org/" target="_blank"><img class="rounded shadow-md" src="~/assets/images/eto-cover-700w.jpg"></a>
+      </div>
+      <p>Щоб дізнатися більше про нашу роботу, будь ласка, завітайте на наш основний сайт (англійською мовою):&nbsp;<a href="https://euroteamoutreach.org/" target="_blank">euroteamoutreach.org</a></p>
+      <hr class="border-b border-grey-light my-12"><button class="btn btn-blue" @click="downloadPdf">
+        <span class="mr-2">У що ми віримо</span><font-awesome-icon :icon="['fas', 'cloud-download-alt']" />
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
