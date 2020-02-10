f01e7b126d86f624bc2ff56dfacef753c8ccff9a
Joshua Steele <joshukraine@gmail.com>
2020-02-10 16:54:55 +0200

    Update "study at home" text.
    * Include language about online option.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="pt-12 md:pt-16">
-    <section class="every-word-fade bg-cover bg-center border-b-8 border-white">
-      <div class="container text-center py-32 px-10">
-        <div class="flex flex-col justify-center pl-3 pt-4 lg:pb-4 lg:pt-16">
+    <section class="bg-center bg-cover border-b-8 border-white every-word-fade">
+      <div class="container px-10 py-32 text-center">
+        <div class="flex flex-col justify-center pt-4 pl-3 lg:pb-4 lg:pt-16">
           <img src="~/assets/svg/bf-logo-full.svg">
-          <div class="text-center mt-10 lg:mt-16">
+          <div class="mt-10 text-center lg:mt-16">
             <button class="btn btn-blue-outline md:btn-lg" @click="linkToEnrollment">
               {{ enrollNow }}
             </button>
@@ -12,9 +12,9 @@
         </div>
       </div>
     </section>
-    <section class="relative pt-10 pb-16 jesus-bg-fade bg-cover bg-center border-b-8 border-white sm:py-16 lg:py-20">
+    <section class="relative pt-10 pb-16 bg-center bg-cover border-b-8 border-white jesus-bg-fade sm:py-16 lg:py-20">
       <div class="absolute inset-x-0 bottom-0 px-6 mb-2 text-center sm:text-right sm:px-2">
-        <p class="text-xs text-grey-darker mb-0">
+        <p class="mb-0 text-xs text-grey-darker">
           {{ coverDisclaimer }}
         </p>
       </div>
@@ -26,7 +26,7 @@
           <h2 class="text-center">
             {{ foundationHeading }}
           </h2>
-          <div class="mb-3 mt-8 mx-auto w-3/4 sm:w-2/3 lg:hidden">
+          <div class="w-3/4 mx-auto mt-8 mb-3 sm:w-2/3 lg:hidden">
             <img class="block" src="https://res.cloudinary.com/euro-team-outreach/image/upload/c_scale,f_auto,q_auto:best,w_500/v1575608311/bibliya/lesson-1-combo-spread_ljvuvy.png">
           </div>
           <!-- eslint-disable-next-line vue/no-v-html -->
@@ -34,24 +34,24 @@
         </div>
       </div>
     </section>
-    <section class="py-10 ark-bg-fade bg-cover bg-center border-b-8 border-white sm:py-16 md:py-24 xl:py-32">
+    <section class="py-10 bg-center bg-cover border-b-8 border-white ark-bg-fade sm:py-16 md:py-24 xl:py-32">
       <div class="container flex mx-auto lg:max-w-5xl">
         <div class="lg:mr-6 lg:w-1/2">
           <h2 class="text-center text-white">
             {{ chronHeading }}
           </h2>
-          <div class="mt-6 mb-2 mx-auto w-3/4 lg:hidden">
+          <div class="w-3/4 mx-auto mt-6 mb-2 lg:hidden">
             <img src="https://res.cloudinary.com/euro-team-outreach/image/upload/c_scale,f_auto,q_auto:best,w_500/v1575608311/bibliya/noah-timeline-combo_ujftea.png">
           </div>
           <!-- eslint-disable-next-line vue/no-v-html -->
           <p class="text-white sm:text-center" v-html="chronContent" />
         </div>
-        <div class="hidden ml-6 lg:block lg:pt-4 w-1/2">
+        <div class="hidden w-1/2 ml-6 lg:block lg:pt-4">
           <img src="https://res.cloudinary.com/euro-team-outreach/image/upload/c_scale,f_auto,q_auto:best,w_500/v1575608311/bibliya/noah-timeline-combo_ujftea.png">
         </div>
       </div>
     </section>
-    <section class="py-10 phil-bg-fade bg-cover bg-center border-b-8 border-white sm:py-16 md:py-24">
+    <section class="py-10 bg-center bg-cover border-b-8 border-white phil-bg-fade sm:py-16 md:py-24">
       <div class="container flex mx-auto lg:max-w-4xl">
         <div class="hidden lg:block lg:mr-16 lg:w-1/3 lg:pt-4 xl:mr-20">
           <img class="block rounded shadow-md" src="https://res.cloudinary.com/euro-team-outreach/image/upload/f_auto,q_auto:best/v1575608387/bibliya/phillip-Bible-700w_haaw5l.jpg">
@@ -60,7 +60,7 @@
           <h2 class="text-center">
             {{ gainIndHeading }}
           </h2>
-          <div class="my-8 mx-auto max-w-xs w-3/4 sm:my-12 lg:hidden">
+          <div class="w-3/4 max-w-xs mx-auto my-8 sm:my-12 lg:hidden">
             <img class="block rounded shadow-md" src="https://res.cloudinary.com/euro-team-outreach/image/upload/f_auto,q_auto:best/v1575608387/bibliya/phillip-Bible-700w_haaw5l.jpg">
           </div>
           <!-- eslint-disable-next-line vue/no-v-html -->
@@ -68,8 +68,8 @@
         </div>
       </div>
     </section>
-    <section class="py-12 study-bg-fade bg-cover bg-center border-b-8 border-white sm:py-16 md:py-24 lg:py-32">
-      <div class="container mx-auto max-w-md lg:max-w-2xl xl:py-16">
+    <section class="py-12 bg-center bg-cover border-b-8 border-white study-bg-fade sm:py-16 md:py-24 lg:py-32">
+      <div class="container max-w-md mx-auto lg:max-w-2xl xl:py-16">
         <h2 class="text-center text-white">
           {{ studyHeading }}
         </h2>
@@ -79,16 +79,16 @@
       </div>
     </section>
     <!-- Call to Action-->
-    <section class="py-12 jac-ladder-bg-fade bg-cover bg-center border-b-8 border-white sm:py-16 md:py-20">
-      <div class="container mx-auto max-w-4xl lg:flex">
-        <div class="mb-6 mx-auto max-w-xs w-1/2 lg:w-1/3 lg:mb-0 lg:mr-10">
+    <section class="py-12 bg-center bg-cover border-b-8 border-white jac-ladder-bg-fade sm:py-16 md:py-20">
+      <div class="container max-w-4xl mx-auto lg:flex">
+        <div class="w-1/2 max-w-xs mx-auto mb-6 lg:w-1/3 lg:mb-0 lg:mr-10">
           <img class="block mx-auto" src="https://res.cloudinary.com/euro-team-outreach/image/upload/c_scale,f_auto,h_400,q_auto:best/v1575608311/bibliya/lesson-1-vertical-ukr-600w_pqpywx.png">
         </div>
         <div class="max-w-md mx-auto lg:flex-1 lg:pt-3">
           <h3 class="text-center text-bf-blue-dark">
             {{ callToAction }}
           </h3>
-          <div class="text-center mt-10 lg:mt-10">
+          <div class="mt-10 text-center lg:mt-10">
             <button class="btn btn-blue-outline sm:btn-lg" @click="linkToEnrollment">
               {{ beginToday }}
             </button>
@@ -112,7 +112,7 @@ export default {
       gainIndHeading: 'Здобудьте свою незалежність',
       gainIndContent: 'Чи Вам доводилось бути в замішанні від того, що Ви намагалися збагнути тлумачення різних конфесій, які суперечать одне одному? Чия тут правда? Як не стати ошуканим? <em>Біблія понад усе</em> навчить Вас самостійно досліджувати Писання, використовуючи прості і випробувані часом літературні прийоми. З кожною новою відповіддю, знайденою для себе, Ви будете здобувати впевненість у тому, що Ваші висновки правильні — і не тому, що хтось так сказав, а тому, що так каже Біблія.',
       studyHeading: 'Навчайтеся вдома',
-      studyContent: 'Без аудиторій, без їзди, без зустрічей. Ми надсилаємо кожен урок Вам додому цілком безплатно. Просто прочитайте урок у своєму темпі, дайте відповіді на запитання і надішліть їх нам! Ми оцінимо Вашу роботу і повернемо ці відповіді разом із наступним уроком.',
+      studyContent: 'Без аудиторій, без їзди, без зустрічей. Ми надсилаємо кожен урок Вам додому цілком безплатно — або поштою, або в онлайн режимі. Просто прочитайте урок у своєму темпі, дайте відповіді на запитання і надішліть їх нам! Ми оцінимо Вашу роботу і повернемо ці відповіді разом із наступним уроком.',
       callToAction: 'Вирушаючи в подорож, під час якої Ви познайомитесь не лише з найбільшою Книгою всіх часів, але й з її Автором, приготуйтесь до того, що Ви будете здивовані, вражені та змінені.',
       beginToday: 'Почніть сьогодні',
     };
